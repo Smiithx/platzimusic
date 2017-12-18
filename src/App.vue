e4259f4474f245c6b537dac69c5a882aa4aa36e9
Andres Gonzalez <smiith17@gmail.com>
2017-12-18 10:40:32 -0400

    Actualizando datos en la interfaz con Vue.js

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   #app
       img(src='./assets/logo.png')
       h1 PlatziMusic
+      select(v-model="selectedCountry")
+        option(v-for="country in countries" :value="country.value") {{country.name}}
       ul
         artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
@@ -13,18 +15,34 @@ export default {
   name: 'app',
   data () {
     return {
-        artists: []
+        artists: [],
+        countries: [
+            { name: 'Venezuela', value:'venezuela'},
+            { name: 'Argentina', value:'argentina'},
+            { name: 'Colombia', value:'colombia'},
+        ],
+        selectedCountry: 'venezuela'
     }
   },
     components: {
       Artist: Artist
     },
-    mounted: function () {
-      const self = this;
-        getArtists()
-            .then(function (artists) {
-                self.artists = artists
-            })
+    mounted() {
+        this.refreshArtists();
+    },
+    methods: {
+      refreshArtists(){
+          const self = this;
+          getArtists(this.selectedCountry)
+              .then(function (artists) {
+                  self.artists = artists
+              })
+      }
+    },
+    watch: {
+        selectedCountry(){
+            this.refreshArtists();
+        }
     }
 }
 </script>
